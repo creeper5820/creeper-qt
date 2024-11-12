553898473c186e70cd876e3be8d3287fc76f1d26
creeper5820 <cqq1960180796@163.com>
2024-11-12 23:30:52 +0800

    add icon to line-edit

--- src/widget/line-edit.hh
+++ src/widget/line-edit.hh
@@ -2,6 +2,7 @@
 
 #include "widget/widget.hh"
 
+#include <qicon.h>
 #include <qlineedit.h>
 
 namespace creeper {
@@ -13,6 +14,9 @@ public:
         : Extension(parent) {
         Extension::setPlaceholderText("你好世界 HelloWorld");
         Extension::setFont(QFont("monospace", 10, QFont::Normal));
+        Extension::addAction(QIcon(":/theme/icon/normal/search.png"),
+            QLineEdit::LeadingPosition);
+        Extension::setAlignment(Qt::AlignVCenter);
         reloadTheme();
     }
 
