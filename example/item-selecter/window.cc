cc246b71f49a753173e81d7dae86c6b5cd676de1
creeper5820 <cqq1960180796@163.com>
2024-12-05 17:48:10 +0800

    update example item selector

--- example/item-selecter/window.cc
+++ example/item-selecter/window.cc
@@ -25,19 +25,29 @@ struct Window::Impl {
     ConcaveSwitchButton switchShowMethod;
     Label switchShowMethodLabel;
 
+    bool singleColumn = true;
+
     void generateExamples() {
-        leftData.emplace_back("BeiJing");
-        leftData.emplace_back("ShangHai");
-        leftData.emplace_back("JiangSu");
-        leftData.emplace_back("JiangXi");
-        leftData.emplace_back("WuHang");
-        leftData.emplace_back("XiAn");
-        leftData.emplace_back("ZheJiang");
-        leftData.emplace_back("NanJing");
-        leftData.emplace_back("LanZhou");
+        leftData.emplace_back("北京");
+        leftData.emplace_back("长沙");
+        leftData.emplace_back("兰州");
+        leftData.emplace_back("南京");
+        leftData.emplace_back("上海");
+        leftData.emplace_back("深圳");
+        leftData.emplace_back("武汉");
+        leftData.emplace_back("西安");
     }
 
+    void updateList() {
+        if (singleColumn) updateListSingleColumn();
+        if (!singleColumn) updateListDoubleColumn();
+    }
+
+private:
     void updateListSingleColumn() {
+        leftVerticalList1.setVisible(false);
+        rightVerticalList1.setVisible(false);
+
         leftVerticalList0.clear();
         for (const auto city : leftData)
             leftVerticalList0.addSimpleLabel(city);
@@ -46,42 +56,66 @@ struct Window::Impl {
             rightVerticalList0.addSimpleLabel(city);
     }
 
-    void updateListDoubleColumn() { }
+    void updateListDoubleColumn() {
+        leftVerticalList1.setVisible(true);
+        rightVerticalList1.setVisible(true);
+
+        leftVerticalList0.clear();
+        leftVerticalList1.clear();
+        for (bool left = false; const auto city : leftData) {
+            if (left) leftVerticalList1.addSimpleLabel(city);
+            else leftVerticalList0.addSimpleLabel(city);
+            left = !left;
+        }
+        rightVerticalList0.clear();
+        rightVerticalList1.clear();
+        for (bool left = false; const auto city : rightData) {
+            if (left) rightVerticalList1.addSimpleLabel(city);
+            else rightVerticalList0.addSimpleLabel(city);
+            left = !left;
+        }
+    }
 };
 
 Window::Window()
     : MainWindow()
     , pimpl_(new Impl) {
-    setFont(QFont { "monospace", 15, QFont::Bold });
+    setFont(QFont { "monospace", 12 });
 
     auto h0 = new QHBoxLayout;
     h0->setAlignment(Qt::AlignCenter);
     h0->addWidget(&pimpl_->switchTheme);
     h0->addWidget(&pimpl_->switchThemeLabel);
-    h0->addWidget(&pimpl_->switchShowMethod);
-    h0->addWidget(&pimpl_->switchShowMethodLabel);
+    auto h1 = new QHBoxLayout;
+    h1->setAlignment(Qt::AlignCenter);
+    h1->addWidget(&pimpl_->switchShowMethod);
+    h1->addWidget(&pimpl_->switchShowMethodLabel);
+
+    auto v1 = new QVBoxLayout;
+    v1->addLayout(h0);
+    v1->addLayout(h1);
 
     auto g0 = new QGridLayout;
-    g0->addWidget(&pimpl_->leftVerticalList0, 0, 0, 4, 2);
-    g0->addWidget(&pimpl_->rightVerticalList0, 0, 3, 4, 2);
+    g0->addWidget(&pimpl_->leftVerticalList0, 0, 0, 4, 1);
+    g0->addWidget(&pimpl_->leftVerticalList1, 0, 1, 4, 1);
+    g0->addWidget(&pimpl_->rightVerticalList0, 0, 3, 4, 1);
+    g0->addWidget(&pimpl_->rightVerticalList1, 0, 4, 4, 1);
     g0->addWidget(&pimpl_->singleLeft2Right, 0, 2, 1, 1);
     g0->addWidget(&pimpl_->allLeft2Right, 1, 2, 1, 1);
     g0->addWidget(&pimpl_->singleRight2Left, 2, 2, 1, 1);
     g0->addWidget(&pimpl_->allRight2Left, 3, 2, 1, 1);
 
     auto v0 = new QVBoxLayout;
-    v0->addLayout(h0);
+    v0->addLayout(v1);
     v0->addLayout(g0);
 
-    pimpl_->switchShowMethodLabel.setText("switch show style");
-    pimpl_->switchShowMethodLabel.setFixedSize(200, 50);
+    pimpl_->switchShowMethodLabel.setText("切换显示方法");
     pimpl_->switchShowMethodLabel.setFont(font());
-    pimpl_->switchThemeLabel.setText("switch theme");
-    pimpl_->switchThemeLabel.setFixedSize(200, 50);
+    pimpl_->switchThemeLabel.setText("切换主题颜色");
     pimpl_->switchThemeLabel.setFont(font());
 
-    pimpl_->switchTheme.setFixedSize({ 100, 50 });
-    pimpl_->switchShowMethod.setFixedSize({ 100, 50 });
+    pimpl_->switchTheme.setFixedSize({ 60, 30 });
+    pimpl_->switchShowMethod.setFixedSize({ 60, 30 });
 
     pimpl_->singleLeft2Right.setText(">");
     pimpl_->singleLeft2Right.setFixedSize(100, 50);
@@ -108,26 +142,63 @@ Window::Window()
         Theme::reloadTheme();
     });
 
+    connect(&pimpl_->leftVerticalList0, &ListWidget::clicked,
+        [this] { pimpl_->leftVerticalList1.clearSelection(); });
+    connect(&pimpl_->leftVerticalList1, &ListWidget::clicked,
+        [this] { pimpl_->leftVerticalList0.clearSelection(); });
+
+    connect(&pimpl_->rightVerticalList0, &ListWidget::clicked,
+        [this] { pimpl_->rightVerticalList1.clearSelection(); });
+    connect(&pimpl_->rightVerticalList1, &ListWidget::clicked,
+        [this] { pimpl_->rightVerticalList0.clearSelection(); });
+
     connect(&pimpl_->singleLeft2Right, &PushButton::released, [this] {
-        const auto selected = pimpl_->leftVerticalList0.currentIndex().row();
-        const auto item = pimpl_->leftVerticalList0.currentItem()->text();
-        if (selected == -1) return;
-
-        pimpl_->leftData.erase(pimpl_->leftData.begin() + selected);
+        const auto selected0 = pimpl_->leftVerticalList0.currentIndex().row();
+        const auto selected1 = pimpl_->leftVerticalList1.currentIndex().row();
+        if (selected0 == -1 && selected1 == -1) return;
+        const auto& focusList
+            = selected0 == -1 ? pimpl_->leftVerticalList1 : pimpl_->leftVerticalList0;
+        const auto item = focusList.currentItem()->text();
+        const auto index = selected0 == -1 ? 2 * (selected1 + 1) - 1 : 2 * selected0;
+        pimpl_->leftData.erase(pimpl_->leftData.begin() + index);
         pimpl_->rightData.push_back(item);
-        pimpl_->updateListSingleColumn();
+        pimpl_->updateList();
     });
-
-    connect(&pimpl_->allLeft2Right, &PushButton::released, [this] { });
-    connect(&pimpl_->singleRight2Left, &PushButton::released, [this] { });
-    connect(&pimpl_->allRight2Left, &PushButton::released, [this] { });
+    connect(&pimpl_->singleRight2Left, &PushButton::released, [this] {
+        const auto selected0 = pimpl_->rightVerticalList0.currentIndex().row();
+        const auto selected1 = pimpl_->rightVerticalList1.currentIndex().row();
+        if (selected0 == -1 && selected1 == -1) return;
+        const auto& focusList
+            = selected0 == -1 ? pimpl_->rightVerticalList1 : pimpl_->rightVerticalList0;
+        const auto item = focusList.currentItem()->text();
+        const auto index = selected0 == -1 ? 2 * (selected1 + 1) - 1 : 2 * selected0;
+        pimpl_->rightData.erase(pimpl_->rightData.begin() + index);
+        pimpl_->leftData.push_back(item);
+        pimpl_->updateList();
+    });
+    connect(&pimpl_->allLeft2Right, &PushButton::released, [this] {
+        pimpl_->rightData.insert(
+            pimpl_->rightData.end(), pimpl_->leftData.begin(), pimpl_->leftData.end());
+        pimpl_->leftData.clear();
+        pimpl_->updateList();
+    });
+    connect(&pimpl_->allRight2Left, &PushButton::released, [this] {
+        pimpl_->leftData.insert(
+            pimpl_->leftData.end(), pimpl_->rightData.begin(), pimpl_->rightData.end());
+        pimpl_->rightData.clear();
+        pimpl_->updateList();
+    });
+    connect(&pimpl_->switchShowMethod, &ConcaveSwitchButton::released, [this] {
+        pimpl_->singleColumn = !pimpl_->singleColumn;
+        pimpl_->updateList();
+    });
 
     auto main = new QWidget;
     main->setLayout(v0);
     setCentralWidget(main);
 
     pimpl_->generateExamples();
-    pimpl_->updateListSingleColumn();
+    pimpl_->updateList();
 }
 
 Window::~Window() { delete pimpl_; }
