103bf1c88b32bcd64dd8788f0bdb4ab58765fe28
creeper5820 <cqq1960180796@163.com>
2025-08-12 13:53:38 +0800

    Develop widgets example

--- example/widgets/main.cc
+++ example/widgets/main.cc
@@ -15,12 +15,22 @@ namespace pro {
     };
     auto font = widget::pro::Font { "WenQuanYi Micro Hei", 12 };
 
-    const auto outlined_button_meta = std::tuple {
+    const auto outlined_button = std::tuple {
         pre::pro::theme,
         pre::pro::font,
         button::pro::FixedSize { 100, 50 },
         button::pro::Radius { -1 },
         button::pro::BorderWidth { 0.5 },
+    };
+    const auto navigation_icons = std::tuple {
+        pre::pro::theme,
+        icon_button::pro::color::STANDARD,
+        icon_button::pro::shape::DEFAULT_ROUND,
+        icon_button::pro::types::DEFAULT,
+        icon_button::pro::width::WIDE,
+        icon_button::pro::FontIcon { material::icon::kCircle },
+        icon_button::pro::Font { material::outlined::font, 20 },
+        icon_button::pro::FixedSize { 80, 50 },
     };
 }
 namespace lambda {
@@ -44,16 +54,38 @@ int main(int argc, char* argv[]) {
         card::pro::Radius { 0 },
 
         card::pro::Layout<Col> {
-            linear::pro::Stretch { 255 },
+            linear::pro::Item<IconButton> {
+                { 0, Qt::AlignHCenter },
+                pre::pro::navigation_icons,
+                icon_button::pro::FontIcon { material::icon::kHome },
+            },
+            linear::pro::Item<IconButton> {
+                { 0, Qt::AlignHCenter },
+                pre::pro::navigation_icons,
+                icon_button::pro::FontIcon { material::icon::kCancel },
+            },
+            linear::pro::Item<IconButton> {
+                { 0, Qt::AlignHCenter },
+                pre::pro::navigation_icons,
+                icon_button::pro::FontIcon { material::icon::kAttachFile },
+            },
+            linear::pro::Item<IconButton> {
+                { 0, Qt::AlignHCenter },
+                pre::pro::navigation_icons,
+                icon_button::pro::FontIcon { material::icon::kDashboard },
+            },
+            linear::pro::Stretch {
+                255,
+            },
             linear::pro::Item<OutlinedButton> {
-                pre::pro::outlined_button_meta,
+                pre::pro::outlined_button,
                 button::pro::Text { "退出程序" },
                 button::pro::Clickable {
                     [] { QApplication::quit(); },
                 },
             },
             linear::pro::Item<OutlinedButton> {
-                pre::pro::outlined_button_meta,
+                pre::pro::outlined_button,
                 button::pro::Text { "颜色模式" },
                 button::pro::Clickable { pre::lambda::change_color },
             },
@@ -75,21 +107,7 @@ int main(int argc, char* argv[]) {
             linear::pro::Item { workspace_navigation },
             linear::pro::Item<FilledCard> {
                 { 255 },
-                card::pro::Layout<Col> {
-                    linear::pro::Item<Image> {
-                        image::pro::Bind { image },
-                        image::pro::ContentScale { ContentScale::CROP },
-                        image::pro::Radius { 15 },
-                        image::pro::Opacity { 1. },
-                        image::pro::BorderWidth { 5. },
-                        image::pro::BorderColor { Qt::red },
-                        image::pro::PainterResource {
-                            "https://c-ssl.duitang.com/uploads/blog/202104/08/"
-                            "20210408220918_a9e0d.jpg",
-                            [&] { image->update(); },
-                        },
-                    },
-                },
+                card::pro::Layout<Col> {},
             },
         },
     };
