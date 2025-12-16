a4d425480201cdecd7723e340b4108296104b5f2
creeper5820 <cqq1960180796@163.com>
2025-12-17 04:54:01 +0800

    Fix compile error on Windows MinGW

--- example/widgets/util/download.cc
+++ example/widgets/util/download.cc
@@ -1,30 +1,5 @@
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <ws2tcpip.h>
-
-#ifdef __MINGW32__
-typedef INT(WSAAPI* PFN_GETADDRINFOEXCANCEL)(HANDLE* CancelHandle);
-static PFN_GETADDRINFOEXCANCEL func_get_addr_info_ex_cancel = nullptr;
-
-static INT WSAAPI GetAddrInfoExCancel(LPHANDLE lpHandle) {
-    if (func_get_addr_info_ex_cancel == nullptr) {
-        auto* wsdll = LoadLibraryW((wchar_t*)"ws2_32.lib");
-        func_get_addr_info_ex_cancel =
-            (PFN_GETADDRINFOEXCANCEL)GetProcAddress(wsdll, "GetAddrInfoExCancel");
-    }
-
-    return func_get_addr_info_ex_cancel(lpHandle);
-}
-#endif
-#endif
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#include "httplib.h"
-#pragma GCC diagnostic pop
-
 #include "download.hh"
+#include "httplib.hh" // IWYU pragma: keep
 #include "proxy.hh"
 
 #include <fstream>
